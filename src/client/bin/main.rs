219ea394c0ba32d9291a98f1e79724cab1f9af4f
Adam Blakney <akblakney@gmail.com>
2023-03-12 14:43:29 -0400

    made help option more accessible

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -17,7 +17,7 @@ fn main() {
 
   // TODO add help and print to stderr
   if args.len() < 2 {
-    println!("invalid command");
+    println!("invalid command, run with --help or help to see options");
     std::process::exit(1);
   }
 
@@ -28,7 +28,7 @@ fn main() {
     query::handle_query(&mut stream);
   } else if command == "rand" {
     rand::handle_rand(&mut stream, &args);
-  } else if command == "help" {
+  } else if command == "help" || command == "-h" || command == "--help" {
     help::handle_help();
   } else {
     println!("nothing to be done");
